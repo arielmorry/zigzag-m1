dbfba05a0f532d5632c3dd8d22a08fed1a1ac319
Ariel Morry <ariel@morry.io>
2020-10-15 14:43:49 +0300

    Alter where clause for order grid to avoid ambiguous on order created date Get all tracking numbers (not just zigzag) Change Tracking Number Title in grid

--- app/code/local/ZigZag/Base/Model/Observer/Grid.php
+++ app/code/local/ZigZag/Base/Model/Observer/Grid.php
@@ -8,25 +8,38 @@ class ZigZag_Base_Model_Observer_Grid
      */
     public function addTrackingToSalesCollection($observer)
     {
-        /** @var Mage_Sales_Model_Resource_Order_Grid_Collection $collection */
-        $collection = $observer->getOrderGridCollection();
-
         $separator = '<br>';
-        $action = Mage::app()->getRequest()->getActionName();
+        $action    = Mage::app()->getRequest()->getActionName();
         if ($action == 'exportCsv' || $action == 'exportExcel') {
             $separator = '|';
         }
 
+        /** @var Mage_Sales_Model_Resource_Order_Grid_Collection $collection */
+        $collection = $observer->getOrderGridCollection();
+        $collection->addFilterToMap('created_at', 'main_table.created_at');
+
         /** @var Zend_Db_Select $select */
         $select = $collection->getSelect();
+
         $select->joinLeft(
             array(
                 'shipment_track' => $collection->getTable('sales/shipment_track'),
             ),
-            'shipment_track.order_id = main_table.entity_id AND shipment_track.carrier_code LIKE "zigzag%"',
-            array('track' => new Zend_Db_Expr('GROUP_CONCAT(DISTINCT shipment_track.track_number SEPARATOR "'. $separator .'")'))
+            'shipment_track.order_id = main_table.entity_id',
+            array('track' => new Zend_Db_Expr('GROUP_CONCAT(DISTINCT shipment_track.track_number SEPARATOR "' . $separator . '")'))
         );
         $select->group('main_table.entity_id');
+
+        if ($where = $select->getPart(Zend_Db_Select::WHERE)) {
+            foreach ($where as $key => $condition) {
+                if (strpos($condition, '`created_at`')) {
+                    $where[$key]   = str_replace('`created_at`', new Zend_Db_Expr('`main_table`.`created_at`'), $condition);;
+                }
+            }
+
+            $select->reset(Zend_Db_Select::WHERE);
+            $select->setPart('where', $where);
+        }
 
         return $this;
     }
